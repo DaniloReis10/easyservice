2edf09cb54cfac6bda0caf54d77bade0612b77db
Eric <Eric@Eric>
2015-10-06 17:12:04 -0300

    Alterações e adição da mounTable

--- Equipe3/js/functionsClientes.js
+++ Equipe3/js/functionsClientes.js
@@ -1,108 +1,77 @@
-$(function()
-{
-	var index = 0;
-	function Adicionar()
-	{
-		index++;
-		$("#tblCadastro tbody")
-				.append(		"<tr>"
-								+ "<td><input type='text' size='16' name='nome"+index+"' placeholder='Nome'/></td>"
-								+ "<td><input type='text'size='16' 	name='email"+index+"' placeholder='exemplo@com'/></td>"
-								+ "<td><input type='text' size='12' name='cpf"+index+"' placeholder='12345678900'/></td>"
-								+ "<td><input type='text'size='12'	name='telefone"+index+"' placeholder='85999887766'/></td>"
-								+ "<td><input type='text'size='16' 	name='endereco"+index+"' placeholder='Av. Abolição'/></td>"
-								+ "<td><input type='text'size='12'	name='cep"+index+"' placeholder='XXXXX-XXX'/></td>"
-								+ "<td><input type='text'size='6'	name='numero"+index+"' placeholder='XX'/></td>"
-								+ "<td><img src='images/disk.png' class='btnSalvar'><img src='images/delete.png' class='btnExcluir'/></td>"
-								+ "</tr>");
+$(document).ready(function()
+		{
+			/* A complex JSON example */
 
-		$(".btnSalvar").bind("click", Salvar);
-		$(".btnExcluir").bind("click", Excluir);
-		inputTextValidation();
-	}
-	;
+			var jsonContentComplex = [ {
+				"index" : 1,
+				"nome" : "Eric",
+				"email" : "eric@gmail.com",
+				"cpf" : "930.549.392-00",
+				"telefone" : "85996180188",
+				"endereco" : "Av. Rua Tal",
+				"cep" : "60165230",
+				"numero" : "455",
+			}, {
+				"index" : 2,
+				"nome" : "Eric",
+				"email" : "eric@gmail.com",
+				"cpf" : "930.549.392-00",
+				"telefone" : "85996180188",
+				"endereco" : "Av. Rua Tal",
+				"cep" : "60165230",
+				"numero" : "455",
+			} ];
 
-	function Editar()
-	{
-		var par = $(this).parent().parent(); // tr
-		var tdNome = par.children(":nth-child(1)").children();
-		var tdEmail = par.children(":nth-child(2)").children();
-		var tdCpf = par.children(":nth-child(3)").children();
-		var tdTel = par.children(":nth-child(4)").children();
-		var tdEndereco = par.children(":nth-child(5)").children();
-		var tdCep = par.children(":nth-child(6)").children();
-		var tdNumero = par.children(":nth-child(7)").children();
-		var tdBotoes = par.children("td:nth-child(8)");
+			/* Activates the complete example */
 
-		tdNome.html("<input type='text' size='16' name='nome"+index+"'/>");
-		tdEmail.html("<input type='text' id='tdEmail' value='" + tdEmail.html()
-				+ "' size='16'/>");
-		tdCpf.html("<input type='text' id='tdCpf' value='" + tdCpf.html()
-				+ "' size='12'/>");
-		tdTel.html("<input type='text' id='tdTel' value='" + tdTel.html()
-				+ "' size='12'/>");
-		tdEndereco.html("<input type='text' id='tdEndereco' value='"
-				+ tdEndereco.html() + "' size='16'/>");
-		tdCep.html("<input type='text' id='tdCep' value='" + tdCep.html()
-				+ "' size='12'/>");
-		tdNumero.html("<input type='text' id='tdNumero' value='"
-				+ tdNumero.html() + "' size='6'/>");
-		tdBotoes.html("<img src='images/disk.png' class='btnSalvar'/>");
+			$('#example2').mounTable(jsonContentComplex, {
 
-		$(".btnSalvar").bind("click", Salvar);
-		$(".btnEditar").bind("click", Editar);
-		$(".btnExcluir").bind("click", Excluir);
-	}
-	;
+				/* The model class */
 
-	function Salvar()
-	{
-		var par = $(this).parent().parent(); // tr
-		var tdNome = par.children(":nth-child(1)").children();
-		var tdEmail = par.children(":nth-child(2)").children();
-		var tdCpf = par.children(":nth-child(3)").children();
-		var tdTel = par.children(":nth-child(4)").children();
-		var tdEndereco = par.children(":nth-child(5)").children();
-		var tdCep = par.children(":nth-child(6)").children();
-		var tdNumero = par.children(":nth-child(7)").children();
-		var tdBotoes = par.children("td:nth-child(8)");
+				model : '.mountable-model',
 
-		tdNome.html(tdNome.children("input[type=text]").val());
-		tdEmail.html(tdEmail.children("input[type=text]").val());
-		tdCpf.html(tdCpf.children("input[type=text]").val());
-		tdTel.html(tdTel.children("input[type=text]").val());
-		tdEndereco.html(tdEndereco.children("input[type=text]").val());
-		tdCep.html(tdCep.children("input[type=text]").val());
-		tdNumero.html(tdNumero.children("input[type=text]").val());
-		tdBotoes
-				.html("<img src='images/delete.png' class='btnExcluir'/><img src='images/pencil.png' class='btnEditar'/>");
+				/* No console messages */
 
-		$(".btnEditar").bind("click", Editar);
-		$(".btnExcluir").bind("click", Excluir);
-	}
-	;
+				noDebug : false,
 
-	function Excluir()
-	{
-		var par = $(this).parent().parent(); // tr
-		par.remove();
-		index--;
-	}
-	;
+				/* Options to your new line button */
 
-	$(".btnEditar").bind("click", Editar);
-	$(".btnExcluir").bind("click", Excluir);
-	$("#btnAdicionar").bind("click", Adicionar);
+				addLine : {
 
-});
+					/* New line button selector */
 
-/*function validFields()
-{
-	var elements = [];
-	var inputs = document.getElementsByTagName("input");
-	for(var i = 0; i < inputs.length; i++) {
-	    if(inputs[i].name.indexOf('name') == 0) {
-	    	elements.push(inputs[i]);
-	    }
-	}
-}*/
+					button : "#button2",
+
+					/* Callback function */
+
+					onClick : function(element)
+					{
+						console.log('Line added!');
+						return true;
+					}
+				},
+
+				/* Options to your delete line button */
+
+				deleteLine : {
+
+					/* Delete line button selector */
+
+					button : ".mountable-remove-line-2",
+
+					/* Callback function */
+
+					onClick : function(element)
+					{
+						if (confirm("Você tem Certeza?"))
+						{
+							console.log('Line removed!');
+							return true;
+						}
+					}
+
+				}
+
+			});
+
+		});
